deb0ab8246e785489a459d7d4554d0f8bdbfa490
myalenti <myalenti@mongodb.com>
2016-04-10 23:39:00 +0000

    Midstep

--- enron2mongo.py
+++ enron2mongo.py
@@ -12,6 +12,7 @@ import json
 import xmltodict
 from collections import OrderedDict
 from pymongo import MongoClient, InsertOne
+import gridfs
 
 logging.basicConfig(level=logging.INFO,
                     format='(%(threadName)4s) %(levelname)s %(message)s',
@@ -21,6 +22,7 @@ xmlSource = sys.argv[1]
 xmlFile = open(xmlSource)
 xmldict = xmltodict.parse(xmlFile)
 xmldict_root = xmldict["Root"]["Batch"]["Documents"]["Document"]
+sourceDir = "/enron/edrm-enron-v2"
 batchSize = 10
 global retries
 retries = 5
@@ -37,6 +39,7 @@ password = sys.argv[3]
 position = 0
 database_name = "enron"
 collection_name = "email"
+jobs = []
 
 dictLength = len(xmldict["Root"]["Batch"]["Documents"]["Document"])
 
@@ -50,16 +53,33 @@ def pretty(messyDict):
 
 def generateMongoDoc(nativeDict, increment):
     mongoDoc = OrderedDict()
-    mongoDoc["DocId"] = nativeDict[increment]["@DocID"]
+    mongoDoc["_id"] = nativeDict[increment]["@DocID"]
     mongoDoc["From"] = extractNamedTag(nativeDict[increment]["Tags"]["Tag"],"#From")
-    print pretty(mongoDoc)
+    mongoDoc["To"] = extractNamedTag(nativeDict[increment]["Tags"]["Tag"],"#To")
+    mongoDoc["Date"] = extractNamedTag(nativeDict[increment]["Tags"]["Tag"],"#DateSent")
+    mongoDoc["Attachment"] = extractNamedTag(nativeDict[increment]["Tags"]["Tag"],"#HasAttachments")
+    mongoDoc["AttachmentName"] = extractNamedTag(nativeDict[increment]["Tags"]["Tag"],"#AttachmentNames")
+    mongoDoc["AttachmentCount"] = extractNamedTag(nativeDict[increment]["Tags"]["Tag"],"#AttachmentCount")
+    mongoDoc["Subject"] = extractNamedTag(nativeDict[increment]["Tags"]["Tag"],"#Subject")
+    mongoDoc["CC"] = extractNamedTag(nativeDict[increment]["Tags"]["Tag"],"#CC")
+    mongoDoc["Body"] = extractEmailBody(nativeDict[increment]["Files"]["File"])
+
+    #print pretty(mongoDoc)
     #print mongoDoc
-    return mongoDoc
+    return mongoDoc 
 
 def extractNamedTag(arrayOfDocs, tagName):
     for doc in arrayOfDocs:
         if doc["@TagName"] == tagName:
             return doc["@TagValue"]
+
+def extractEmailBody(arrayOfDocs):
+    for doc in arrayOfDocs:
+        if doc["@FileType"] == "Text":
+            emailFile = sourceDir + "/" + doc["ExternalFile"]["@FilePath"] + "/" + doc["ExternalFile"]["@FileName"]
+            email_file = open(emailFile,"r")
+            emailBody = email_file.read()
+    return emailBody
 
 def batchSaveToDB(requestList):
     results = col.bulk_write(requestList, ordered=True)
@@ -69,6 +89,7 @@ def batchSaveToDB(requestList):
 conn = mongoConnector()
 db = conn[database_name]
 col = db[collection_name]
+grid = gridfs.GridFS(db)
 
 print dictLength
 #print pretty(xmldict["Root"]["Batch"]["Documents"]["Document"][0] )
@@ -106,12 +127,18 @@ while position != dictLength:
                 continue
             requestList.append(InsertOne(generateMongoDoc(xmldict_root, position)))
             position = position + 1
-        results = batchSaveToDB(requestList)
-        #print results
-        #print requestList
-        print results.bulk_api_result
-        print results.acknowledged
+        if len(requestList) > 0 :
+            results = batchSaveToDB(requestList)
+            #print results
+            #print requestList
+            print results.bulk_api_result
+            print results.acknowledged
         requestList = []
-            
 print pretty(xmldict_root[0])
+'''            
+position = 0
+while position != dictLength:
         
+   if xmldict_root[position]["DocType"] == "File" :
+        filePath =  sourceDir + "/" + xmldict_root[position]["Tag"]["Tags"]
+'''
